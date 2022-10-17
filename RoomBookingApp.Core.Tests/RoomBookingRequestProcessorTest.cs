f097e8678eda7667cc83fd83d2bab0046d97f85c
weonidas <lelowb@hotmail.com>
2022-10-17 16:00:59 -0300

    First Test Implemented

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,33 @@
+﻿using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomBookingApp.Core
+{
+    public class RoomBookingRequestProcessorTest
+    {
+        [Fact]
+        public void Should_Return_Room_Booking_Response_With_Request_Values()
+        {
+            var request = new RoomBookingRequest
+            {
+                FullName = "Test Name",
+                Email = "test@mail.com",
+                Date = new DateTime(2022, 10, 15)
+            };
+
+            var processor = new RoomBookingRequestProcessor();
+
+            RoomBookingResult result = processor.BookRoom(request);
+
+            Assert.NotNull(result);
+            Assert.Equal(request.FullName, result.FullName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+        }
+    }
+}
---
+++ RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
@@ -0,0 +1,21 @@
+﻿using RoomBookingApp.Core.Models;
+
+namespace RoomBookingApp.Core.Processors
+{
+    public class RoomBookingRequestProcessor
+    {
+        public RoomBookingRequestProcessor()
+        {
+        }
+
+        public RoomBookingResult BookRoom(RoomBookingRequest request)
+        {
+            return new RoomBookingResult
+            {
+                Date = request.Date,
+                Email = request.Email,
+                FullName = request.FullName
+            };
+        }
+    }
+}
