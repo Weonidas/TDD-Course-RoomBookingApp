efd0771869c8068570f39c36131e22a653aa93f7
weonidas <lelowb@hotmail.com>
2022-10-18 21:36:20 -0300

    Implemented Api Controller tests

---
+++ RoomBookingApp.Api.Tests/WeatherForecastControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using RoomBookingApp.Api.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomBookingApp.Api.Tests
+{
+    public class WeatherForecastControllerTest
+    {
+        [Fact]
+        public void Should_Return_Forecast_Results()
+        {
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(loggerMock.Object);
+
+            var result = controller.Get();
+
+            Assert.NotNull(result);
+            Assert.True(result.Count() > 1, "The amount was not greater than 0.");
+        }
+    }
+}
---
+++ RoomBookingApp.Api/Controllers/RoomBookingController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RoomBookingApp.Core.Enums;
+using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+
+namespace RoomBookingApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomBookingController : ControllerBase
+    {
+        private readonly IRoomBookingRequestProcessor _processor;
+
+        public RoomBookingController(IRoomBookingRequestProcessor processor)
+        {
+            _processor = processor;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BookRoom(RoomBookingRequest bookingRequest)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = _processor.BookRoom(bookingRequest);
+                if (result.ResultFlag == BookingResultFlag.Success)
+                    return Ok(result);
+                else
+                    ModelState.AddModelError(nameof(RoomBookingRequest.Date), "No Rooms Available for this specific Date");
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
